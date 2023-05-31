5f204ff0beb2ef6f1895f9d15fca83dd2810e50a
cioby09 <claudiociobanu09@gmail.com>
2023-05-31 21:56:14 +0200

    exercise completed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Hello</h1>
+    <h1 class="text-center">Movies</h1>
+    <div class="comic-container py-4">
+        <div class="container">
+            <div class="row row-cols-5 g-3">
+                @foreach ($movies as $movie)
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <img src="{{ $movie->image }}" alt="">
+                                <h5 class="card-title">{{ $movie->title }}</h5>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 @endsection
